38638ceeece988225c939a9cc5ac9516e024b731
Robin JR <robin.jr@triassicsolutions.com>
2018-03-14 20:50:58 +0530

    [Robin JR] [14 Mar 2018] [Join Facebook screen design]

--- Facebook/App.js
+++ Facebook/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import JoinFacebookScreen from './Script/JoinFacebook'
+import Example from './Script/Example'
 export default class App extends React.Component {
   render() {
     return (
