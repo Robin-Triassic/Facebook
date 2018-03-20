79aebc3f1ff6acc6cd8fe7ed7c47fecb25ba3c2a
Mohammed Janish <mohammed.janish@triassicsolutions.com>
2018-03-20 11:57:56 +0530

    [Janish] [MainScreenUI-change]

--- Facebook/App.js
+++ Facebook/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import JoinFacebookScreen from './Script/JoinFacebook'
 import Example from './Script/Example'
+import SignUp from './Script/SignUp'
+import SignUpName from './Script/SignUpName'
+import SignUpGender from './Script/signUpGender'
+
 export default class App extends React.Component {
   render() {
     return (
-      <JoinFacebookScreen/>
+      <SignUpGender/>
     );
   }
 }
